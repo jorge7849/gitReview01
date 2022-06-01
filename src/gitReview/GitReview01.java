45dd7d699835cd42a3688508dd75879e25f6c399
jorge7849 <jorgesimbron01@yahoo.com>
2022-05-31 21:53:13 -0400

    2nd commit

--- src/gitReview/GitReview01.java
+++ src/gitReview/GitReview01.java
@@ -3,8 +3,11 @@ package gitReview;
 public class GitReview01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Today we learn Github");
+		System.out.println("today is a very good day");
+		System.out.println("today is a very good day");
 	}
+
+	
 
 }
